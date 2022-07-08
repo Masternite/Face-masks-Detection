1815e1acbef35795d9724ae47736f3c29823f4d8
Masternite <88206043+Masternite@users.noreply.github.com>
2022-07-08 21:45:20 +0530

    Add files via upload

---
+++ lesson1.cpp
@@ -0,0 +1,15 @@
+/
+#include "/usr/local/include/opencv2/objdetect.hpp"
+#include "/usr/local/include/opencv2/highgui.hpp"
+#include "/usr/local/include/opencv2/imgproc.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+void main() {
+
+	string path = "resources/test.png";
+	Mat img = imread(path);
+	imshow()
+}
